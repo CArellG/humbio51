77797f545b45ce59199bdeabb400a263d69103e1
annashcherbina <annashch@stanford.edu>
2019-09-26 23:45:41 -0700

    config updates

--- setup_2019/create_kubernetes_cluster.sh
+++ setup_2019/create_kubernetes_cluster.sh
@@ -5,37 +5,58 @@
 #gcloud auth application-default login
 #gcloud config set project gbsc-gcp-class-humbio51-aut19
 
-#gcloud container clusters create \
-#  --machine-type n1-standard-4 \
-#  --num-nodes 1 \
-#  --zone us-central1-b \
-#  --cluster-version latest \
-#  humbio51-2019
+gcloud container clusters create \
+  --machine-type n1-standard-4 \
+  --num-nodes 2 \
+  --zone us-central1-b \
+  --cluster-version latest \
+  --disk-size=40Gi \
+  humbio51-2019
 
-#kubectl create clusterrolebinding cluster-admin-binding \
-#   --clusterrole=cluster-admin \
-#  --user=annashch@stanford.edu
+kubectl create clusterrolebinding cluster-admin-binding \
+   --clusterrole=cluster-admin \
+  --user=annashch@stanford.edu
+
+
+gcloud beta container node-pools create user-pool \
+  --machine-type n1-standard-8 \
+  --num-nodes 1 \
+  --enable-autoscaling \
+  --min-nodes=0 \
+  --max-nodes=40 \
+  --node-labels hub.jupyter.org/node-purpose=user \
+  --node-taints hub.jupyter.org_dedicated=user:NoSchedule \
+  --zone us-central1-b \
+  --disk-size=20Gi \
+  --cluster humbio51-2019
+
 
 ##setup helm & tiller -- to install/uninstall easily,  refer here: https://medium.com/@pczarkowski/easily-install-uninstall-helm-on-rbac-kubernetes-8c3c0e22d0d7
 
 #curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-#kubectl --namespace kube-system create serviceaccount tiller
-#kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-#helm init --service-account tiller --wait
+kubectl --namespace kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller --wait
 #kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
 #helm version
 
 ## Install jupyterhub
 #openssl rand -hex 32
 ## add secretToken to config.yaml
-#helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
-#helm repo update
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
+
+
+#kubectl create -f namespace_jhub.yaml
+#kubectl get namespace
+#kubectl --namespace jhub apply -f data_pvc.yaml 
+#kubectl --namespace jhub get pvc
 
 export RELEASE=jhub
 export NAMESPACE=jhub
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
      --namespace $NAMESPACE  \
-     --values config.yaml
+     --values config.canvas.yaml
 
 
 ##Your release is named jhub and installed into the namespace jhub.
