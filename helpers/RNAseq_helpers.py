9e92abcee69ede4006e5f14b2824f090fffbac61
Anna Shcherbina <annashcherbina@gmail.com>
2019-10-20 00:53:17 +0000

    in class activity 9 updates

--- helpers/RNAseq_helpers.py
+++ helpers/RNAseq_helpers.py
@@ -6,22 +6,23 @@ def load_rnaseq_data(systems_subset,
     data=pd.read_csv(
         filepath_or_buffer=filepath,
         header=0,
-        sep='\t')
+        index_col=0,
+        sep='\t').transpose()
     batches= pd.read_csv(
              filepath_or_buffer=batches,
              header=0,
+             index_col=0,
              sep='\t')
+    nrow_data,ncol_data=data.shape
+    nrow_batches,ncol_batches=data.shape 
+    merged_df=pd.merge(data, batches, left_index=True,right_index=True)
     if systems_subset=="all":
-        data=data[batches.sort_values(by="System")["Sample"]]
-        #data.columns=batches.sort_values(by="System")["System"]
-        return data,batches
-    else: 
-        batches_subset=batches.loc[batches['System'].isin(systems_subset)]
-        data_subset=data.loc[:,batches_subset['Sample']]
-        batches_subset=batches_subset.reset_index()
-        data_subset=data_subset[batches_subset.sort_values(by="System")["Sample"]]
-        #data_subset.columns=batches_subset.sort_values(by="System")["System"]
-        return data_subset,batches_subset
-
-    
+        data_filtered=merged_df.iloc[:,0:ncol_data].transpose()
+        batches_filtered=merged_df.iloc[:,0:-1*ncol_batches]
+    else:
+        samples_to_keep=merged_df['System'].isin(systems_subset)
+        merged_df_subset=merged_df[samples_to_keep]
+        data_filtered=merged_df_subset.iloc[:,0:ncol_data].transpose()
+        batches_filtered=merged_df_subset.iloc[:,0:-1*ncol_batches]
+    return data_filtered,batches_filtered
 
