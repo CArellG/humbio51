aa047fd36a98a8135eae0d81b78ad63778218bbe
Anna Shcherbina <annashcherbina@gmail.com>
2021-06-17 16:43:47 -0700

    functional version

--- setup_2021/update.canvas.v8.sh
+++ setup_2021/update.canvas.v8.sh
@@ -1,1 +1,1 @@
-helm install --namespace jhub jhub jupyterhub/jupyterhub --version=0.8.2 --values config.canvas.yaml
+helm upgrade --namespace jhub jhub jupyterhub/jupyterhub --version=0.8.2 --values config.canvas.yaml
